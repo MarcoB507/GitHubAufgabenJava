3b26ae78ada2ae29440f00b6dc5dd9614cfabb61
mbart <mbarth509@gmail.com>
2022-08-23 09:53:26 +0200

    aufgabe6

--- src/aufgabe6/Ratespiel.java
+++ src/aufgabe6/Ratespiel.java
@@ -1,4 +1,20 @@
 package aufgabe6;
 
+
 public class Ratespiel {
+    public static void main(String args[]) {
+        int min = 0;
+        int max = 100;
+        int wunschzahl = 0;
+
+        do {
+
+            System.out.println("Random Zahl von " + min + "bis" + max + ":");
+            int random_zahl = (int) Math.floor(Math.random() * (max - min + 1) + min);
+            System.out.println(random_zahl);
+        }
+            while (wunschzahl != 70);
+
+        System.out.println("GG");
+        }
 }
