d1c62ed3b9ac701026144d13d9192840396a5313
mbart <mbarth509@gmail.com>
2022-08-25 09:59:57 +0200

    aufgabe14_solution

--- src/aufgabe14/ArrayWrapper.java
+++ src/aufgabe14/ArrayWrapper.java
@@ -1,23 +1,75 @@
 package aufgabe14;
 
+import java.util.Arrays;
+
 public class ArrayWrapper {
     int zahlen[];
-    int i;
-    int x;
+
+    private int elementCount;
+
+
+
 
     public ArrayWrapper() {
-        zahlen = new int[10];
+        zahlen = new int[2];
+        elementCount = 0;
     }
 
-//    //mehrZahlen == größeres Array
-//    int mehrZahlen[] = new int[zahlen.length + 1];
-//
-//    //alte zahlen ins neue Array
-//    for(i = 0; i < zahlen.length; i++){
-//        mehrZahlen[i] = zahlen[i];
-//
-//        mehrZahlen[zahlen.length] = x;
-//        return mehrZahlen;
-//    }
-//
+    public ArrayWrapper(int zahlengroesse) {
+        zahlen = new int[zahlengroesse];
+        elementCount = 0;
+    }
+
+    @Override
+    public String toString() {
+        return "ArrayWrapper{" +
+                "zahlen=" + Arrays.toString(zahlen) +
+                ", elementCount=" + elementCount +
+                '}';
+    }
+
+    public void add(int z) {
+
+        if(elementCount >= zahlen.length) {
+
+            int[] newzahlen = new int[zahlen.length * 2];
+            kopie(newzahlen);
+            zahlen = newzahlen;
+        }
+        zahlen[elementCount] = z;
+        elementCount ++;
+
+    }
+
+    private void kopie(int[] neuesArray){
+        for (int i = 0; i < zahlen.length; i ++){
+            neuesArray[i] = zahlen[i];
+        }
+    }
+
+    public int getElement(int index){
+
+        return zahlen[index];
+    }
+
+    public int getArraySize(){
+
+       return zahlen.length;
+    }
+
+    public int getElementCount(){
+
+//        anznewElements = mehrZahlen.length;
+            return elementCount;
+    }
+
+
+    public static void main(String[] args) {
+
+        ArrayWrapper aw = new ArrayWrapper();
+        for (int x = 1; x <= 100; x++){
+            aw.add(x);
+            System.out.println(aw);
+        }
+    }
 }
--- test/aufgaben/aufgaben14/ArrayWrapperTest.java
+++ test/aufgaben/aufgaben14/ArrayWrapperTest.java
@@ -1,11 +1,42 @@
 package aufgaben.aufgaben14;
 
+import aufgabe14.ArrayWrapper;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ArrayWrapperTest {
 
     @Test
-    public void ArrayWrapperTrue(){
+    public void should_add_new_element(){
 
+        ArrayWrapper aw = new ArrayWrapper();
+        aw.add(1);
+        aw.add(2);
+
+        assertEquals(1, aw.getElement(0));
+        assertEquals(2, aw.getElement(1));
+
+     }
+    @Test
+    public void should_double_ArraySize(){
+
+        ArrayWrapper aw = new ArrayWrapper();
+        aw.add(1);
+        aw.add(2);
+        aw.add(3);
+
+        int size = aw.getArraySize();
+
+        assertEquals(4, size);
+
+    }
+    @Test
+    public void should_return_elementCount(){
+        ArrayWrapper aw = new ArrayWrapper();
+        aw.add(5);
+
+        int myelements = aw.getElementCount();
+        assertEquals(1, myelements);
     }
 }
